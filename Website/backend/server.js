1b51cdc2cead6fd894bf8725d166bebad6e8ff54
plocnm <22022139@vnu.edu.vn>
2025-03-30 21:10:13 +0700

    Revert "add start.js1"
    This reverts commit 3d12f7c174a34a1e6abc16d1134c6564cee0addf.

--- Website/backend/server.js
+++ Website/backend/server.js
@@ -34,8 +34,7 @@ app.use('/api/hotels', hotelRoutes);
 app.use('/api/flights', flightRoutes);
 app.use('/api/payments', paymentRoutes);
 
-// Force port 5500
-const PORT = 5500;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
     console.log('CORS enabled for all origins');
